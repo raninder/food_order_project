582abdba3c2d42e9f83a94a6fb41d7bf55c5303b
Hyunsu <ksoksl0711@gmail.com>
2021-11-27 18:09:08 -0800

    modify attributes

--- db/schema/04_orders.sql
+++ db/schema/04_orders.sql
@@ -4,5 +4,8 @@ CREATE TABLE orders (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   quantity SMALLINT NOT NULL,
-  total_price INTEGER NOT NULL
+  total_price INTEGER NOT NULL,
+  order_created_at TIMESTAMP,
+  order_accepted_at TIMESTAMP,
+  ready_at TIMESTAMP
 );
--- db/schema/05_order_histories.sql
+++ db/schema/05_order_histories.sql
@@ -4,8 +4,5 @@ CREATE TABLE order_histories (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   order_id INTEGER REFERENCES orders(id) ON DELETE CASCADE,
-  order_requested_at TIMESTAMP,
-  order_accepted_at TIMESTAMP,
-  ready_at TIMESTAMP,
   picked_up_at TIMESTAMP
 );
