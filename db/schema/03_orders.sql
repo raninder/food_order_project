253be385f0d1389e413085464b79419e6425a608
Hyunsu <ksoksl0711@gmail.com>
2021-11-28 13:59:39 -0800

    modify column names line 8, 9

--- db/schema/03_orders.sql
+++ db/schema/03_orders.sql
@@ -5,7 +5,7 @@ CREATE TABLE orders (
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   quantity SMALLINT NOT NULL,
   total_price INTEGER NOT NULL,
-  order_created_at TIMESTAMP,
-  order_accepted_at TIMESTAMP,
+  created_at TIMESTAMP,
+  accepted_at TIMESTAMP,
   ready_at TIMESTAMP
 );
