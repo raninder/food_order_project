21a2c72f02baea660074c9ed6e96a8213afc3320
Hyunsu <ksoksl0711@gmail.com>
2021-11-28 13:28:41 -0800

    modify route widgets -> owner

--- server/server.js
+++ server/server.js
@@ -33,12 +33,12 @@ app.use(express.static("public"));
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("../routes/users");
-const widgetsRoutes = require("../routes/widgets");
+const ownerRoutes = require("../routes/owner");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/users", usersRoutes(db));
-app.use("/api/widgets", widgetsRoutes(db));
+app.use("/owner", ownerRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
 // Home page
