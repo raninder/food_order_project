c2b66680b6a2261dbcc4c9bef4cbc0ca5492ad1a
Hyunsu <ksoksl0711@gmail.com>
2021-11-28 18:08:51 -0800

    create new table order_details

---
+++ db/schema/04_order_details.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS order_details CASCADE;
+
+CREATE TABLE order_details (
+  id SERIAL PRIMARY KEY NOT NULL,
+  order_id INTEGER REFERENCES orders(id) ON DELETE CASCADE,
+  food_id INTEGER REFERENCES foods(id) ON DELETE CASCADE,
+  quantity INTEGER NOT NULL
+);
