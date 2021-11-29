627a0f27d55ad88433ddbe53e8465826c74668b9
Hyunsu <ksoksl0711@gmail.com>
2021-11-28 18:07:02 -0800

    modify database, delete tables that we don't need

--- db/schema/05_order_histories.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS order_histories CASCADE;
-
-CREATE TABLE order_histories (
-  id SERIAL PRIMARY KEY NOT NULL,
-  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  order_id INTEGER REFERENCES orders(id) ON DELETE CASCADE,
-  picked_up_at TIMESTAMP
-);
