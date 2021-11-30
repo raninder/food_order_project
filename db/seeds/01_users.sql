85233639d56603eb9dde2e83668094c76fb9a829
Hyunsu <ksoksl0711@gmail.com>
2021-11-29 20:12:46 -0800

    create fake data for testing

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,4 +1,12 @@
--- Users table seeds here (Example)
 INSERT INTO users (name, phone, email, created_at)
-VALUES ('Alice', 7785551124, 'alice2@gmail.com', CURRENT_TIMESTAMP);
--- INSERT INTO users (name) VALUES ('Kira');
+VALUES ('Raninder', 1234567890, 'raninder@gmail.com', Now()),
+('Daniel', 09876564321, 'daniel@gmail.com', Now()),
+('Hyunsu', 7785551124, 'hyunsu@gmail.com', Now()),
+('Eavan', 0185298066, 'eavan@gmail.com', Now()),
+('Alice', 9962480450, 'alice@gmail.com', Now()),
+('Robert', 8545521821, 'robert@gmail.com', Now()),
+('Andy', 1242354301, 'andy@gmail.com', Now()),
+('Conor', 4545687424, 'conor@gmail.com', Now()),
+('Andrew', 4564564565, 'andrew@gmail.com', Now()),
+('Bianka', 4753121004, 'bianka@gmail.com', Now()),
+('Jack', 777242272555, 'jack@gmail.com', Now());
---
+++ db/seeds/02_foods.sql
@@ -0,0 +1,10 @@
+INSERT INTO foods ( name, photo, estimated_time, price )
+VALUES ('pizza', 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg/330px-Eq_it-na_pizza-margherita_sep2005_sml.jpg', 15, 10),
+('hamburger', 'https://assets.epicurious.com/photos/57c5c6d9cf9e9ad43de2d96e/master/pass/the-ultimate-hamburger.jpg', 10, 8),
+('butter chicken', 'https://www.cubesnjuliennes.com/wp-content/uploads/2020/06/Best-Instant-Pot-Butter-Chicken-Recipe.jpg', 15, 12),
+('fried chicken', 'https://insanelygoodrecipes.com/wp-content/uploads/2020/04/Fried_Chicken-1024x536.png', 15, 15),
+('pasta', 'https://www.simplyhappyfoodie.com/wp-content/uploads/2017/09/instant-pot-meatball-pasta-dinner-4-500x375.jpg', 12, 14),
+('poutine', 'https://iamhomesteader.com/wp-content/uploads/2020/04/poutine-5.jpg', 10, 11),
+('ginger ale', 'http://cdn.shopify.com/s/files/1/0521/4525/8669/products/shutterstock_1579168372.jpg?v=1610844621', 0, 2),
+('sprite', 'https://img.prairiedogbrewing.ca/wp-content/uploads/2020/06/30095346/Sprite-scaled-lg.jpg', 0, 2),
+('coke', 'https://i0.wp.com/okobagels.com/wp-content/uploads/2021/02/Coke.jpg?fit=614%2C461&ssl=1', 0, 2);
