75fa6d3cf46e41f1cbd79e25b20ae3960012ebad
Hyunsu <ksoksl0711@gmail.com>
2021-11-29 19:49:38 -0800

    add reminder SMS

--- sms.js
+++ sms.js
@@ -1,37 +1,49 @@
 const twilio = require('twilio');
 const  { accountSid, authToken } = require('./lib/sms');
-console.log("twilio:" , accountSid, authToken);
 const client = new twilio(accountSid, authToken);
 
-
+// send SMS to owner when there's a new order
 const sendNewOrder = () => {
   client.messages
     .create({
       body: 'New Order has been sent, Please check it out',
-      from: '+1',
-      to: '+1'
+      from: '+1360515',
+      to: '+1778901'
     })
     .then(message => console.log(message.sid));
 };
 exports.sendNewOrder = sendNewOrder;
 
-const confirmedOrder = () => {
+// send SMS to user when owner confirm the order
+const confirmedOrder = (time) => {
   client.messages
     .create({
-      body: 'Your order is confirmed, Estimated time is 20 mins',
-      from: '+1',
-      to: '+1'
+      body: `Your order is confirmed, Estimated time is ${time} mins`,
+      from: '+13605',
+      to: '+1778903'
     })
-    .then(message => console.log(message.sid));
+    .then(() => {
+      // send reminder SMS to user when order will be ready in 5 mins
+      setTimeout(() => {
+        client.messages
+          .create({
+            body: `Your order will be ready in 5 mins`,
+            from: '+136051',
+            to: '+1778903'
+          })
+          .then(message => console.log(message.sid));
+      }, (time * 1000 * 60) - (5 * 1000 * 60));
+    });
 };
 exports.confirmedOrder = confirmedOrder;
 
+// send SMS when order is ready to pick up
 const readyToPickUp = () => {
   client.messages
     .create({
       body: 'Your order is ready to pick up',
-      from: '+1',
-      to: '+1'
+      from: '+13605154640',
+      to: '+1778903'
     })
     .then(message => console.log(message.sid));
 };
