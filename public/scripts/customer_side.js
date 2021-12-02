fd9794fa27ddc7fc10e65d9025d6038e7cb5d1fd
Hyunsu <ksoksl0711@gmail.com>
2021-12-01 19:30:47 -0800

    add click event that can place food in order list

--- public/scripts/customer_side.js
+++ public/scripts/customer_side.js
@@ -1,36 +1,29 @@
-// const add_to_cart = function(photo) {
-//   console.log("item:", photo);
-//   const selectedimage = `<img class="dish_img" src= ${photo}/>`
-//   $(".chosen_items").append(selectedimage);
-// }
-
-
-
 $(document).ready(function() {
 
   // this creates the food picture and price
 
   const create_picture_element = (item) =>  {
     const {id, name, photo, price} = item;
-    console.log("this is id and photo:" , id, photo);
-    let html = 
+    // console.log("this is id and photo:" , id, photo);
+    let html =
     `<div class="dish_and_price">
-      <div class= "dish dish_${id}">         
-        <img class="dish_img" src="${photo}"/>
+      <div class= "dish dish_${id}">
+        <img class="dish_img" id="photo_${id}" src="${photo}"/>
       </div>
         <div class="dish_name_price">
-          <div class="dish_name">${name}</div>
-          <div class="dish_price">${price}</div>
+          <div class="dish_name" id="dish_name_${id}">${name}</div>
+          <div class="dish_price" id="dish_price_${id}">${price}</div>
         </div>
     </div>`;
-    return html
-  }
+    return html;
+  };
 
   const render_picture = (arr) => {
     for (const i of arr) {
-      $(".foods").append(create_picture_element(i))
+      $(".foods").append(create_picture_element(i));
     }
-  }
+    attachClickHandler();
+  };
 
   const $load_menu = () => {
     $.ajax({
@@ -38,29 +31,41 @@ $(document).ready(function() {
       method: "GET",
       dataType: "json"
     })
-    .then((data) => {
-      render_picture(data);
-
-
-    });
+      .then(data => {
+        render_picture(data);
+        return data;
+      });
   };
 
-let shopping_cart = [];
+  const attachClickHandler = () => {
+    $(".dish_img").click(event => {
+      console.log(event);
+      const curElementId = event.currentTarget.id;
+      const uniqueId = curElementId.substring(6);
+      const dishNameId = '#dish_name_' + uniqueId;
+      const dishPriceId = '#dish_price_' + uniqueId;
+      const dishName = $(dishNameId).text();
+      const dishPrice = $(dishPriceId).text();
+      const dishImg = event.currentTarget.currentSrc;
+      const dishData = {dishName, dishPrice, dishImg};
+      renderOrders(dishData);
+    });
 
-  // $(".foods").on("click", ".dish_and_price",  function() {
-    // event.preventDefault();
-    // console.log($(this))
+  };
 
-
-    // $(".food_picked").append($(".dish_img"))
-    // console.log($(this).find(".dish_price").html())
-  // })
-
-  // $("div")
-  // $("#id")
-  // $(".className")
-  // $(this)
-
+  const renderOrders = (dishData) => {
+    let food =
+    `<div class= "order_item">
+    <img class="order_food_image" src="${dishData.dishImg}">
+    <div class="order_item_name_quantity_price">
+      <div class= "order_item_name">${dishData.dishName}</div>
+      <div class="order_item_quantity_price">
+        <div class= "order_item_price">$${dishData.dishPrice}</div>
+        <div class= "order_item_quantity">&nbsp x 3 </div>
+      </div>
+    </div>
+    </div>`;
+    $(".order_area_items").append(food);
+  };
   $load_menu();
-
 });
