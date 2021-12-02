554b0d1afaec50b0892d4c8a00903f993bbb4ae9
Hyunsu <ksoksl0711@gmail.com>
2021-12-01 16:43:18 -0800

    add jquery for order status

---
+++ public/scripts/order_status.js
@@ -0,0 +1,58 @@
+$(document).ready(() => {
+
+  const $orderStatus = order => {
+    const {order_id, confirmed_at, ready_at, picked_up_at} = order;
+    let statusMessage = '';
+
+    // change order status message
+    if (confirmed_at === null) {
+      statusMessage = "Restuarant hasn't confirmed your order";
+    } else if (ready_at === null) {
+      statusMessage = 'Restuarant confirmed your order, your order is now preparing';
+    } else if (picked_up_at === null) {
+      statusMessage = "Your order is ready to pick up";
+    }
+
+    let $status =
+    `<h3>Your Order Status</h3>
+    <h4>Order Number : ${order_id}</h4>
+    <span class="status"><span>${statusMessage}<span></span>`;
+    return $status;
+  };
+  // if user hasn't placed any order yet
+  const $noOrderHistory = () => {
+    let $status = `<h3>We can not find your order</h3>`;
+    return $status;
+  };
+
+  const $renderStatus = orders => {
+    orders.forEach(order => {
+      if (order.picked_up_at === null && order.created_at !== null) {
+        $(".order_status").empty();
+        $(".order_status").append($orderStatus(order));
+      } else {
+        $(".order_status").empty();
+        $(".order_status").append($noOrderHistory());
+      }
+    });
+  };
+
+  // getting user id from url
+  const userId = Number($(location).attr('pathname').substring(7));
+
+  const $loadOrderStatus = () => {
+    $.ajax({
+      url: `/users/${userId}/order/status`,
+      method: "GET",
+      dataType: "json"
+    })
+      .then(data => $renderStatus(data));
+  };
+
+  $loadOrderStatus();
+
+  $(".order_status").hide();
+  $(".your_order").click(() => {
+    $(".order_status").toggle("slow");
+  });
+});
